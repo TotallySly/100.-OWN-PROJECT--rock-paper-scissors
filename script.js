ca4e09254256d1004f9bb1364b330ad4afe12f76
Scott Lyon <82440653+TotallySly@users.noreply.github.com>
2023-03-30 17:17:30 +0200

    Finished dark mode javascript

--- script.js
+++ script.js
@@ -7,21 +7,23 @@ const btnLightMode = document.getElementById('btnLightMode')
 
 const containersDarkMode = document.querySelectorAll('div.--container')
 
-toggleDarkMode.addEventListener('click', () => {
-    btnDarkMode.classList.toggle('--hide-content')
-    documentBody.classList.toggle('--dark-mode')
-    btnLightMode.classList.toggle('--hide-content')
+function darkModeToggler() {
+    toggleDarkMode.addEventListener('click', () => {
+        btnDarkMode.classList.toggle('--hide-content')
+        documentBody.classList.toggle('--dark-mode')
+        btnLightMode.classList.toggle('--hide-content')
 
-    containersDarkMode.forEach((containerDarkMode) => {
-        if (btnLightMode.classList.contains('--hide-content')) {
-            containerDarkMode.style.backgroundColor = '#d2d3db'
-            console.log('true')
-        } else {
-            containerDarkMode.style.backgroundColor = '#484b6a'
-            console.log('false')
-        }
+        containersDarkMode.forEach((containerDarkMode) => {
+            if (btnLightMode.classList.contains('--hide-content')) {
+                containerDarkMode.style.backgroundColor = '#d2d3db'
+            } else {
+                containerDarkMode.style.backgroundColor = '#484b6a'
+            }
+        })
     })
-})
+}
+
+darkModeToggler()
 
 /* 
     TODO
