7299bd5fbdc4fcf07c5f24703d3979f2c5bfae52
Scott Lyon <82440653+TotallySly@users.noreply.github.com>
2023-04-01 11:05:43 +0200

    Add comments to the JS to explain each line. Changed the HTML semantics of div to section where relevant

--- script.js
+++ script.js
@@ -1,23 +1,34 @@
-// A variable to get the body of the document
+// A variable to get the body of the document.
 const documentBody = document.body
-// A variable to get the body of the document
+// A variable to get the container of where the dark/light mode buttons are.
 const toggleDarkMode = document.querySelector('.container__dark-mode-toggler-btns')
+// A variable to get the dark mode button.
 const btnDarkMode = document.getElementById('btnDarkMode')
+// A variable to get the light mode button.
 const btnLightMode = document.getElementById('btnLightMode')
+// A variable to create a node list of all 'sections' with the '.--container class'.
+const containersLightOrDarkMode = document.querySelectorAll('section.--container')
 
-const containersDarkMode = document.querySelectorAll('div.--container')
-
+// A function to toggle between light/dark mode
 function darkModeToggler() {
+    // An event listener that reacts to a mouse click.
     toggleDarkMode.addEventListener('click', () => {
+        // Toggles between adding or removing the '--hide-content' class on the dark mode button.
         btnDarkMode.classList.toggle('--hide-content')
+        // Toggles between adding or removing the '--dark-mode' class on the body.
         documentBody.classList.toggle('--dark-mode')
+        // Toggles between adding or removing the '--hide-content' class on the light mode button.
         btnLightMode.classList.toggle('--hide-content')
 
-        containersDarkMode.forEach((containerDarkMode) => {
-            if (btnLightMode.classList.contains('--hide-content')) {
-                containerDarkMode.style.backgroundColor = '#d2d3db'
+        // A for each method to iterate through the node list of all 'sections' that container the class of '--container'.
+        containersLightOrDarkMode.forEach((containerLightOrDarkMode) => {
+            // An If statement checking if the dark mode button contains the '--hide-content' class.
+            if (btnDarkMode.classList.contains('--hide-content')) {
+                // If it does, add the light mode background colour for the containers
+                containerLightOrDarkMode.style.backgroundColor = '#484b6a'
             } else {
-                containerDarkMode.style.backgroundColor = '#484b6a'
+                // else, add the dark mode background colour for the containers
+                containerLightOrDarkMode.style.backgroundColor = '#d2d3db'
             }
         })
     })
