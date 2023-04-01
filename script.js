98dd1f23987c38fc676003f522693191ebfe2fbb
Scott Lyon <82440653+TotallySly@users.noreply.github.com>
2023-04-01 12:26:30 +0200

    Rewriting the JS functions as I am using the prefers color scheme property within the css

--- script.js
+++ script.js
@@ -9,32 +9,76 @@ const btnLightMode = document.getElementById('btnLightMode')
 // A variable to create a node list of all 'sections' with the '.--container class'.
 const containersLightOrDarkMode = document.querySelectorAll('section.--container')
 
-// A function to toggle between light/dark mode
-function darkModeToggler() {
-    // An event listener that reacts to a mouse click.
-    toggleDarkMode.addEventListener('click', () => {
-        // Toggles between adding or removing the '--hide-content' class on the dark mode button.
-        btnDarkMode.classList.toggle('--hide-content')
-        // Toggles between adding or removing the '--dark-mode' class on the body.
+function checkPrefersDarkMode() {
+    if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+        btnLightMode.classList.add('--hide-content')
+        btnDarkMode.classList.remove('--hide-content')
         documentBody.classList.toggle('--dark-mode')
-        // Toggles between adding or removing the '--hide-content' class on the light mode button.
-        btnLightMode.classList.toggle('--hide-content')
-
-        // A for each method to iterate through the node list of all 'sections' that container the class of '--container'.
-        containersLightOrDarkMode.forEach((containerLightOrDarkMode) => {
-            // An If statement checking if the dark mode button contains the '--hide-content' class.
-            if (btnDarkMode.classList.contains('--hide-content')) {
-                // If it does, add the light mode background colour for the containers
-                containerLightOrDarkMode.style.backgroundColor = '#484b6a'
-            } else {
-                // else, add the dark mode background colour for the containers
-                containerLightOrDarkMode.style.backgroundColor = '#d2d3db'
-            }
-        })
-    })
+    } else {
+        btnLightMode.classList.remove('--hide-content')
+        btnDarkMode.classList.add('--hide-content')
+        documentBody.classList.toggle('--dark-mode')
+    }
 }
 
+function darkModeToggler() {
+    if (btnDarkMode.classList.contains('--hide-content')) {
+        toggleDarkMode.addEventListener('click', () => {
+            // btnDarkMode.classList.add('--hide-content')
+            // btnLightMode.classList.remove('--hide-content')
+            btnDarkMode.classList.toggle('--hide-content')
+            btnLightMode.classList.toggle('--hide-content')
+        })
+    } else {
+        // btnDarkMode.classList.add('--hide-content')
+        // btnLightMode.classList.remove('--hide-content')
+        btnDarkMode.classList.toggle('--hide-content')
+        btnLightMode.classList.toggle('--hide-content')
+    }
+}
+
+checkPrefersDarkMode()
 darkModeToggler()
+// // A function to toggle between light/dark mode
+// function darkModeToggler() {
+//     // An event listener that reacts to a mouse click.
+//     toggleDarkMode.addEventListener('click', () => {
+//         checkPrefersDarkMode()
+//         // // Toggles between adding or removing the '--hide-content' class on the dark mode button.
+//         // btnDarkMode.classList.toggle('--hide-content')
+//         // // Toggles between adding or removing the '--dark-mode' class on the body.
+//         // documentBody.classList.toggle('--dark-mode')
+//         // // Toggles between adding or removing the '--hide-content' class on the light mode button.
+//         // btnLightMode.classList.toggle('--hide-content')
+
+//         // A for each method to iterate through the node list of all 'sections' that container the class of '--container'.
+//         containersLightOrDarkMode.forEach((containerLightOrDarkMode) => {
+//             // An If statement checking if the dark mode button contains the '--hide-content' class.
+//             if (btnLightMode.classList.contains('--hide-content')) {
+//                 // If it does, add the light mode background colour for the containers
+//                 containerLightOrDarkMode.style.backgroundColor = '#484b6a'
+//             } else {
+//                 // else, add the dark mode background colour for the containers
+//                 containerLightOrDarkMode.style.backgroundColor = '#d2d3db'
+//             }
+//         })
+//     })
+// }
+
+// function checkPrefersDarkMode() {
+//     if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+//         btnLightMode.classList.remove('--hide-content')
+//         btnDarkMode.classList.add('--hide-content')
+//         documentBody.classList.toggle('--dark-mode')
+//     } else {
+//         btnLightMode.classList.add('--hide-content')
+//         btnDarkMode.classList.remove('--hide-content')
+//         documentBody.classList.toggle('--dark-mode')
+//     }
+// }
+
+// darkModeToggler()
+// checkPrefersDarkMode()
 
 /* 
     TODO
