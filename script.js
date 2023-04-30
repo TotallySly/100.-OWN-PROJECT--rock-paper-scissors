65b6dae58b2c8e8928ce59553bd04511f1ed703c
Scott Lyon <82440653+TotallySly@users.noreply.github.com>
2023-04-30 15:18:24 +0200

    Work

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
+const togglerBtn = document.querySelector('.container__dark-mode-toggler')
+const darkModeBtn = document.getElementById('turnOnDarkMode')
+const lightModeBtn = document.getElementById('turnOnLightMode')
 
+togglerBtn.addEventListener('click', () => {
+    lightModeBtn.classList.toggle('--hide-content')
+    console.log('CLICK')
+})
